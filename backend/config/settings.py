182ff977e1af1c654df9690cd85cd99c4320fb78
samuel <manukhalifa@hotmail.com>
2026-04-05 17:33:49 +0200

    first commit

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,12 +51,12 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "app.urls"
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "..", "frontend", "pages")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "app.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
@@ -116,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "..", "frontend", "static", "styles")
+]
--- backend/etl/main.py
+++ backend/etl/main.py
@@ -1,12 +1,11 @@
 from etl.etl_files.extract import extract
 from etl.etl_files.transform import transform
-from etl.etl_files.load import load
 import csv
 import pandas as pd
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-df = pd.read_csv(os.path.join(BASE_DIR, "csv_data", "world_cities_sample.csv"))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+df = pd.read_csv(os.path.join(BASE_DIR, "data", "world_cities_sample.csv"))
 cities = df["name_en"].to_list()
 
 def main(cities):    
@@ -15,7 +14,7 @@ def main(cities):
 
     raw_data = extract(cities)
     transformed_data = transform(raw_data)
-    with open(os.path.join(BASE_DIR, "csv_data", "weather_data.csv"), "w") as csvfile:
+    with open(os.path.join(BASE_DIR, "data", "weather_data.csv"), "w") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=transformed_data[0].keys())
         writer.writeheader()
         writer.writerows(transformed_data)
